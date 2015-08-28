40cf714afd3c9001052ae682a1201c9b0faa57a5
edyluisrey <eaguirre378@gmail.com>
2015-08-28 09:56:52 -0500

    add cast examples

---
+++ src/oca/oo/casting/CastTest.java
@@ -0,0 +1,26 @@
+package oca.oo.casting;
+
+class Animal {
+	void makeNoise() {
+		System.out.println("generic noise"); 
+	}
+}
+
+class Dog extends Animal {
+	void makeNoise() {System.out.println("bark"); }
+	void playDead() { System.out.println("roll over"); }
+}
+
+class CastTest {
+	public static void main(String [] args) {
+	    Animal [] a = {new Animal(), new Dog(), new Animal() };
+		for(Animal animal : a) {
+		  animal.makeNoise();
+		  if(animal instanceof Dog) {
+			  Dog d = (Dog) animal;
+			  d.playDead();
+    	   }
+		}
+
+	}
+}
---
+++ src/oca/oo/polymorphism/TestAnimals.java
@@ -0,0 +1,32 @@
+package oca.oo.polymorphism;
+
+//Overrides
+class Animal {
+	public void eat() {
+	  System.out.println("Generic Animal Eating Generically");
+	}
+}
+
+class Horse extends Animal {
+	public void eat() {
+	   System.out.println("Horse eating hay, oats, " + "and horse treats");
+	}
+	public void buck() { }
+}
+
+class cow extends Animal{
+	
+}
+public class TestAnimals {
+	public static void main (String [] args) {
+		Animal a = new Animal();
+		Animal b = new Horse(); // Animal ref, but a Horse object
+		a.eat(); // Runs the Animal version of eat()
+		b.eat(); // Runs the Horse version of eat()
+		//b.buck();  undefined for Animal
+		
+		Animal c = new cow();
+		c.eat();
+				
+	}
+}
