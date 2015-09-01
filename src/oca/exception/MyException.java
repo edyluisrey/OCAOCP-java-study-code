526a0cd8f863317b2156f05194e3d91d9e606a1d
edyluisrey <eaguirre378@gmail.com>
2015-08-31 23:21:04 -0500

    add arrays

---
+++ src/oca/exception/MyException.java
@@ -0,0 +1,20 @@
+package oca.exception;
+
+import java.io.IOException;
+
+class MyException extends Exception {
+	void someMethod () throws MyException {
+	   doStuff();
+	}
+	
+	void doStuff() throws MyException {
+		try {
+			
+			throw new MyException();
+		}
+		catch(MyException me) {
+			throw me;
+		}
+	}
+	
+}
