fe0f7e289326d468474a03dd22df4cbc4b563a84
edyluisrey <eaguirre378@gmail.com>
2015-09-02 12:01:55 -0500

    add more exception example

--- src/oca/exception/Flipper.java
+++ src/oca/exception/Flipper.java
@@ -3,6 +3,7 @@ package oca.exception;
 public class Flipper {
   public static void main(String[] args) {
 	String o = "-";
+	final String s="bob";
 	System.out.println("FRED".toLowerCase().substring(0,3));
 	switch("FRED".toLowerCase().substring(1,4)) {
 		case "yellow":
@@ -11,6 +12,7 @@ public class Flipper {
 			o += "r";
 		case "green":
 			o += "g";
+		case s: ;	
 	}
 	
 	System.out.println(o);
