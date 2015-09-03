38ea5edda8ee9ad57ec0a4974112eed431e53204
edyluisrey <eaguirre378@gmail.com>
2015-09-03 11:09:35 -0500

    add string test(equals,==)

--- src/oca/classes/objects/StaticConstructorTest.java
+++ src/oca/classes/objects/StaticConstructorTest.java
@@ -7,7 +7,31 @@ public class StaticConstructorTest {
     {
         /*Below: I'm trying to create an object of the class
         which would intern call the constructor*/
-		StaticConstructor obj = new StaticConstructor();
-		StaticConstructor ob2 = new StaticConstructor();
+		//StaticConstructor obj = new StaticConstructor();
+		//StaticConstructor ob2 = new StaticConstructor();
+		
+		//Test strings
+		int c=0;
+		StringBuilder sb1 = new StringBuilder("hello");
+		StringBuilder sb2 = new StringBuilder("hello");
+		StringBuilder sb3 = new StringBuilder(sb1);
+		
+		String s1="world";
+		String s2=s1;
+		String s3="world";
+		
+		if(sb1.equals(sb3))
+			c++;
+		if(sb1.equals(sb2))
+			c++;
+		if(sb1==sb2)
+			c++;
+		if(sb1==sb3)
+			c++;
+		if(s1.equals(s3))
+			c++;
+		if(s1.equals(s2))
+			c++;
+		System.out.println(c);	
     }
 }
