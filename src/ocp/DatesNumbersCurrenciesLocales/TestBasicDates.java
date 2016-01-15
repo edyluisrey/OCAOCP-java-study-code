783bb1a25019f43aa2bea566fd8cac18204eaa84
edyluisrey <eaguirre378@gmail.com>
2016-01-14 19:22:28 -0500

    add Dates examples

--- src/ocp/DatesNumbersCurrenciesLocales/TestBasicDates.java
+++ src/ocp/DatesNumbersCurrenciesLocales/TestBasicDates.java
@@ -1,10 +1,17 @@
 package ocp.DatesNumbersCurrenciesLocales;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class TestBasicDates {
      public static void main(String[] args) {
     	 Date d1 = new Date(1_000_000_000_000L); // a trillion, Java 7 style
     	 System.out.println("1st date " + d1.toString());
+    	 
+    	 Calendar c = Calendar.getInstance();
+    	 c.setTime(d1);
+    	 c.add(Calendar.MONTH, 1); 
+    	 Date d2 = c.getTime(); 
+    	 System.out.println("new date " + d2.toString() );
 	 }
 }
