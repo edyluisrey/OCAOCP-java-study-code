8a59ba302f881e7783426e8d4732a0b653de246c
edyluisrey <eaguirre378@gmail.com>
2016-01-25 21:02:48 -0500

    create directory

--- src/ocp/filesio/TestPath.java
+++ src/ocp/filesio/TestPath.java
@@ -10,9 +10,13 @@ public class TestPath {
 	public static void main(String[] args) {
 		 Path path = Paths.get("file1.txt");	
 		 System.out.println(Files.exists(path));
+		 Path  path1 = Paths.get("source");
+		 Path   file = Paths.get("source/file2.txt");
 		 try {
 			Files.createFile(path); //You call static methods on Files rather than instance methods on File.
 			System.out.println(Files.exists(path));
+			Files.createDirectory(path1);
+			Files.createFile(file);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
