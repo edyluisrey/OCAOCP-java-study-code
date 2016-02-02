08092efa23ef63476043d47bb8d03c2bcd44a185
edyluisrey <eaguirre378@gmail.com>
2016-02-02 08:56:01 -0500

    add example BufferedReader

--- src/ocp/filesio/TestIOFile.java
+++ src/ocp/filesio/TestIOFile.java
@@ -1,6 +1,9 @@
 package ocp.filesio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TestIOFile {
 
@@ -11,7 +14,18 @@ public class TestIOFile {
             dir.mkdir();
             File file = new File(dir, "file3");
             file.createNewFile();
-		} catch (Exception x) { } 
+		} catch (Exception x) { 
+			
+		} 
+		
+		//test BufferedReader
+		String s;
+		try {
+		  FileReader fr = new FileReader("fileWrite.txt");
+		  BufferedReader br = new BufferedReader(fr);
+		  while((s = br.readLine()) != null)
+			  System.out.println(s);
+		} catch (IOException e) { }
 	}
 
 }
