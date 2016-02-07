cd39cf085ef1790004cb677ee97dc7a416eada98
edyluisrey <eaguirre378@gmail.com>
2016-02-06 22:47:07 -0500

    add compositions example IS-A, HAS-A

---
+++ src/ocp/oodp/composition/TestAnimal.java
@@ -0,0 +1,19 @@
+package ocp.oodp.composition;
+
+interface Swimmer{
+	void swim();
+}
+
+interface Flying {
+	void fly();
+}
+
+class Duck implements Swimmer{
+	public void swim(){
+		
+	}
+}
+
+public class TestAnimal {
+
+}
