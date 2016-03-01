4fcc5d8efe2b880dd17cc6ef02bdc8b96ba8982f
edyluisrey <eaguirre378@gmail.com>
2016-02-29 22:55:10 -0500

    override hascode method

--- src/ocp/collections/FruitHashEqual.java
+++ src/ocp/collections/FruitHashEqual.java
@@ -1,9 +1,11 @@
 package ocp.collections;
 
 class FruitCitric{
+	int id;
 	String name;
 	String group;
-	FruitCitric(String name, String group){
+	FruitCitric(int id, String name, String group){
+		this.id=id;
 		this.name=name;
 		this.group=group;
 	}
@@ -14,14 +16,23 @@ class FruitCitric{
 	    if(!(o instanceof FruitCitric)) 
 	    	return false;
 	    FruitCitric other = (FruitCitric) o;
-	    return this.group == other.group;
+	    return this.id == other.id;
 	}
+	
+
+	@Override
+	public int hashCode() {
+		return this.id*10;
+	}
+ 
 }
 
 public class FruitHashEqual {
       public static void main(String[] args) {
-    	  FruitCitric orange= new FruitCitric("Orange","Citric");
-    	  FruitCitric tangerine= new FruitCitric("Tangerine","Citric");
+    	  FruitCitric orange= new FruitCitric(3,"Orange","Citric");
+    	  FruitCitric tangerine= new FruitCitric(3,"Tangerine","Citric");
     	  System.out.println(orange.equals(tangerine));
+    	  System.out.println(orange.hashCode());
+    	  System.out.println(tangerine.hashCode());
 	  }
 }
