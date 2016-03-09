eaa6e4ba7dde319133022ecb760693c125b42c8d
edyluisrey <eaguirre378@gmail.com>
2016-03-08 21:02:19 -0500

    add ListIterator example

---
+++ src/ocp/collections/ListIteratorExample.java
@@ -0,0 +1,18 @@
+package ocp.collections;
+
+import java.util.ArrayList;
+
+public class ListIteratorExample {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+		list.add("five");
+		list.add("six");
+		list.add("seven");
+		list.add("one");
+
+	}
+}
