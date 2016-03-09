34b24437755c8f0ff7d1c45ce0addf84da5c437f
edyluisrey <eaguirre378@gmail.com>
2016-03-08 21:38:48 -0500

    add test case for  ListIterator example

--- src/ocp/collections/ListIteratorExample.java
+++ src/ocp/collections/ListIteratorExample.java
@@ -1,6 +1,7 @@
 package ocp.collections;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class ListIteratorExample {
 	public static void main(String[] args) {
@@ -12,7 +13,22 @@ public class ListIteratorExample {
 		list.add("five");
 		list.add("six");
 		list.add("seven");
-		list.add("one");
+		
+		ListIterator<String> it = list.listIterator();
+		
+		while(it.hasNext())
+			System.out.println(it.next());
+		
+		System.out.println("previous:");		
+		while(it.hasPrevious())
+			System.out.println(it.previous());
+		
+		System.out.println("next():"); 		
+		System.out.println("Next element is : " + it.next());
+		
+		System.out.println("Index:");
+		System.out.println("Previous Index is : " + it.previousIndex());  
+		System.out.println("Next Index is : " + it.nextIndex());
 
 	}
 }
