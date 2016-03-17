253cc6c1bbc615e812aba634da4bdfb56698b4a5
edyluisrey <eaguirre378@gmail.com>
2016-03-17 00:14:01 -0500

    add test case for NavigableMap

--- src/ocp/collections/NavigableMapTest.java
+++ src/ocp/collections/NavigableMapTest.java
@@ -1,5 +1,25 @@
 package ocp.collections;
 
-public class NavigableMapTest {
+import java.util.TreeMap;
 
+public class NavigableMapTest {
+    public static void main(String[] args) {
+    	TreeMap<Object,Object> navigableMap=new TreeMap<Object, Object>();
+    	navigableMap.put("C", "Cabbage"); 
+    	navigableMap.put("B", "Broccoli");
+    	navigableMap.put("A", "Asparagus");
+    	navigableMap.put("O", "Onion");
+    	navigableMap.put("L", "Lagumes");
+    	
+    	System.out.println(navigableMap);
+    	System.out.println("Descending Set:"+navigableMap.descendingKeySet());	
+    	System.out.println("Reverse Map :"+navigableMap.descendingMap());
+    	System.out.println("Floor Entry:"+navigableMap.floorEntry("L"));    		
+    	System.out.println("First Entry  :"+navigableMap.firstEntry());    		
+    	System.out.println("Last Key:"+navigableMap.lastKey());    		
+    	System.out.println("First Key :"+navigableMap.firstKey());
+    		
+    		
+
+	}
 }
