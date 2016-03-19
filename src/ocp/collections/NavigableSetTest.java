871c5f0c8d869f8c6a8a54e1a980e30a43538355
edyluisrey <eaguirre378@gmail.com>
2016-03-18 23:12:30 -0500

    add test case for NavigableSet

--- src/ocp/collections/NavigableSetTest.java
+++ src/ocp/collections/NavigableSetTest.java
@@ -1,5 +1,19 @@
 package ocp.collections;
 
-public class NavigableSetTest {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
+public class NavigableSetTest {
+   public static void main(String[] args) {
+	   NavigableSet<String> navigableSet = new TreeSet<String>(Arrays.asList("Orange", "Apple", "Banana", "Avocado", "Coconuts"));
+	   Iterator<String>  iterator = navigableSet.descendingIterator();
+	   while(iterator.hasNext())
+		   System.out.println(iterator.next());
+	   System.out.println(navigableSet.headSet("Banana")); //[Apple, Avocado]
+	   System.out.println(navigableSet.floor("Apple"));
+	   System.out.println(navigableSet.pollLast());  //Orange
+	   
+   }
 }
